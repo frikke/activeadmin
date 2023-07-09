7dc8207e6b856f7168fffb7bc36aea9b66d05a8f
Javier Julio <jjfutbol@gmail.com>
2023-07-08 20:51:22 -0400

    Replace datepicker with date input type

--- app/assets/javascripts/active_admin/base.js
+++ app/assets/javascripts/active_admin/base.js
@@ -176,19 +176,6 @@
     return CheckboxToggler;
   }();
   $.widget.bridge("checkboxToggler", CheckboxToggler);
-  (function($) {
-    $(document).on("focus", "input.datepicker:not(.hasDatepicker)", function() {
-      var input = $(this);
-      if (input[0].type === "date") {
-        return;
-      }
-      var defaults = {
-        dateFormat: "yy-mm-dd"
-      };
-      var options = input.data("datepicker-options");
-      input.datepicker($.extend(defaults, options));
-    });
-  })(jQuery);
   var DropdownMenu = function() {
     function DropdownMenu(options, element) {
       this.options = options;
